abee9ca129b83c2c5c9367d7b0bece7ee0c1216f
Getrudegetty <100869131+Getrudegetty@users.noreply.github.com>
2022-03-04 10:39:18 +0300

    Add files via upload

---
+++ rotating vans/st.js
@@ -0,0 +1,36 @@
+//this is counsole statement//
+/*document.write('hellow you')
+alert('pop up')
+console.log('oh my');
+*/
+
+// const myPeople = [
+//    { Name:"bobo", age: 20, position:"developer"},
+// { Name:"bob", age: 25, position:"designer"},
+//     { Name: "getty" , age: 40, position:"boss"},
+// ];
+    
+// const youngpeople = myPeople.filter(function (person) {
+//     return person.age >= 25;
+// })
+// console.log(youngpeople);
+
+
+
+      const productBox = document.getElementById("productBox")
+
+      function flipRight(){
+        productBox.style.transform = "rotatey(180deg)"
+      }  
+      function flipLeft(){
+        productBox.style.transform = "rotatey(0deg)"
+      }
+   
+
+
+
+
+
+
+
+
